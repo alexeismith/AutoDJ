a97a526c2fd7355fd575775adfc51365ac81c9f0
Alexei <aads500@york.ac.uk>
2021-04-28 15:41:32 +0100

    Implement track sorting algorithms

--- Source/TrackSorter.cpp
+++ Source/TrackSorter.cpp
@@ -7,10 +7,41 @@
 
 #include "TrackSorter.hpp"
 
+#include <random>
+
+enum SortCategory : int
+{
+    bpm,
+    key
+};
+
 
 void TrackSorter::sort()
 {
+    sortCategory = SortCategory::bpm;
+    sortedBpm.sort(*this);
     
+    sortCategory = SortCategory::key;
+    sortedKey.sort(*this);
+    
+    DBG("BPM...");
+    for (auto* track : sortedBpm)
+        DBG(track->bpm);
+
+    DBG("Key...");
+    for (auto* track : sortedKey)
+        DBG(track->key);
+    
+//    DBG("");
+//
+//    std::default_random_engine generator;
+//    std::normal_distribution<double> distribution;
+//
+//    DBG(distribution(generator));
+//    DBG(distribution(generator));
+//    DBG(distribution(generator));
+//    DBG(distribution(generator));
+//    DBG(distribution(generator));
     
     sorted = true;
 }
@@ -26,8 +57,135 @@ TrackInfo TrackSorter::chooseTrack(MixDirection direction)
 
 void TrackSorter::addAnalysed(TrackInfo* track)
 {
-    tracksAnalysed.add(track);
+    const juce::ScopedLock sl(lock);
     
-    if (!sorted) return;
+    if (!sorted)
+    {
+        sortedBpm.add(track);
+        sortedKey.add(track);
+    }
+    else
+    {
+        addIntoSortedArray(sortedBpm, SortCategory::bpm, track);
+        addIntoSortedArray(sortedKey, SortCategory::key, track);
+    }
+}
+
+
+int TrackSorter::compareElements(TrackInfo* first, TrackInfo* second)
+{
+    double firstValue = getSortValue(first);
+    double secondValue = getSortValue(second);
     
+    return compare(firstValue, secondValue);
+}
+
+
+int TrackSorter::compare(double first, double second)
+{
+    int result = first > second;
+    if (first < second) return -1;
+    return result;
+}
+
+
+void TrackSorter::addIntoSortedArray(juce::Array<TrackInfo*>& sortedArray, int category, TrackInfo* track)
+{
+    int result;
+    sortCategory = category;
+    int index = findIndexOf(sortedArray, category, getSortValue(track), result);
+    if (result == 1)
+        index += 1;
+    sortedArray.insert(index, track);
+    
+    DBG("Array...");
+    for (auto* track : sortedArray)
+        DBG(getSortValue(track));
+}
+
+
+// Adapted from https://gist.github.com/adamski/8d1c2719bcf0827b44a4
+int TrackSorter::findIndexOf(const juce::Array<TrackInfo*>& sortedArray, int category, double value)
+{
+    if (sortedArray.size() == 0) return 0;
+    
+    sortCategory = category;
+    
+    // Conduct binary search
+    int low, high, index;
+    high = sortedArray.size();
+    low = 0;
+    double valueAtIndex;
+    index = (high + low) / 2;
+
+    juce::String debug;
+
+    while (high != low+1)
+    {
+        valueAtIndex = getSortValue(sortedArray.getUnchecked(index));
+        
+        if (value == valueAtIndex)
+        {
+            return index;
+        }
+        else if (valueAtIndex < value)
+        {
+            low = index;
+        }
+        else
+        {
+            high = index;
+        }
+        index = (high + low) / 2;
+    }
+
+    // If there is an element above the current index, check whether it is closer to value
+    if (sortedArray.size() > index+1)
+    {
+        double valueAboveIndex = getSortValue(sortedArray.getUnchecked(index+1));
+        
+        if (abs(valueAboveIndex - value) < abs(valueAtIndex - value))
+            index += 1;
+    }
+    
+    return index;
+}
+
+
+int TrackSorter::findIndexOf(const juce::Array<TrackInfo*>& sortedArray, int category, double value, int& result)
+{
+    if (sortedArray.size() == 0)
+    {
+        result = 0;
+        return 0;
+    }
+    
+    int index = findIndexOf(sortedArray, category, value);
+    
+    double valueAtIndex = getSortValue(sortedArray.getUnchecked(index));
+    
+    result = compare(value, valueAtIndex);
+        
+    return index;
+}
+
+
+void TrackSorter::remove(TrackInfo* const* track)
+{
+    sortedBpm.remove(track);
+}
+
+
+double TrackSorter::getSortValue(TrackInfo* track)
+{
+    switch (sortCategory)
+    {
+        case SortCategory::bpm:
+            return track->bpm;
+        case SortCategory::key:
+            return track->key;
+        default:
+            jassert(false); // Unrecognized sortCategory
+            return 0.0;
+    };
 }
--- Source/TrackSorter.hpp
+++ Source/TrackSorter.hpp
@@ -15,10 +15,13 @@
 
 typedef struct MixDirection
 {
-    double tempo = 0;
-    double energy = 0;
-    int currentKey = 0;
+    double tempoCurrent;
+    double tempoChange;
+    double energyCurrent;
+    double energyChange;
+    int keyCurrent = 0;
 } MixDirection;
+
 
 class TrackSorter
 {
@@ -34,15 +37,30 @@ public:
     
     void addAnalysed(TrackInfo* track);
     
+    const juce::Array<TrackInfo*>& getSortedBpm() { return sortedBpm; }
+    const juce::Array<TrackInfo*>& getSortedKey() { return sortedKey; }
+    
+    int compareElements(TrackInfo* first, TrackInfo* second);
+    
 private:
     
-    void remove(TrackInfo* track);
+    int compare(double first, double second);
+    
+    void addIntoSortedArray(juce::Array<TrackInfo*>& sortedArray, int category, TrackInfo* track);
+    
+    int findIndexOf(const juce::Array<TrackInfo*>& sortedArray, int category, double value);
+    
+    int findIndexOf(const juce::Array<TrackInfo*>& sortedArray, int category, double value, int& result);
+    
+    void remove(TrackInfo* const* track);
+    
+    double getSortValue(TrackInfo* track);
     
     juce::CriticalSection lock;
     
     bool sorted = false;
+    int sortCategory;
     
-    juce::Array<TrackInfo*> tracksAnalysed;
     juce::Array<TrackInfo*> sortedBpm;
     juce::Array<TrackInfo*> sortedKey;
     

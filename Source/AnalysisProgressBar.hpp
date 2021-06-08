465816bc6e9bd159d333d5f28574a8b30c5afc21
Alexei <aads500@york.ac.uk>
2021-06-08 21:37:27 +0100

    Document AnalysisProgressBar.hpp

--- Source/AnalysisProgressBar.hpp
+++ Source/AnalysisProgressBar.hpp
@@ -13,6 +13,10 @@
 #include "AnalysisManager.hpp"
 
 
+/**
+ Custom extension of JUCE progress bar, showing the audio analysis progress.
+ Allows the user to pause/resume analysis.
+ */
 class AnalysisProgressBar : public juce::ProgressBar
 {
 public:
@@ -46,30 +50,36 @@ public:
     @param[in] e Information about the input event that occured, such as mouse position */
     void mouseDown(const juce::MouseEvent& e) override;
     
+    /** Updates the analysis progress level.
+     
+     @param[in] progress Fraction indicating current analysis progress (0.0 to 1.0) */
     void update(double progress);
     
+    /** Toggles play/pause state and notifies AnalysisManager. */
     void playPause();
     
+    /** Forces a pause state and notifies AnalysisManager. */
     void pause();
     
 private:
     
+    /** Updates the text displayed in the progress bar, based on its current state. */
     void updateText();
     
     juce::CriticalSection lock; ///< RAII lock to ensure thread-safety while acessing data within this class
     
     AnalysisManager* analysisManager = nullptr; ///< Pointer to the audio analysis manager
     
-    double progress = 0.0;
+    double progress = 0.0; ///< Fraction to track current analysis progress (0.0 to 1.0)
     
-    bool paused = false;
+    bool paused = false; ///< Flag to track play/pause state of analysis
     
-    bool mouseOver = false;
+    bool mouseOver = false; ///< Flag to track whether the mouse is hovering over this component
     
-    juce::Rectangle<float> imageArea;
-    
-    juce::Image playImg;
-    juce::Image pauseImg;
+    juce::Image playImg; ///< Play icon to indicate possible interaction when the mouse is over this component (while analysis is paused)
+    juce::Image pauseImg; ///< Pause icon to indicate possible interaction when the mouse is over this component (while analysis is active)
+    juce::Rectangle<float> imageArea; ///< Area in which to render the play/pause icon
+
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AnalysisProgressBar) ///< JUCE macro to add a memory leak detector
 };

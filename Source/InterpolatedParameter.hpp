2fe15251e902861f61213a639c62f6c86536136d
Alexei <aads500@york.ac.uk>
2021-06-09 02:19:56 +0100

    Document Track and InterpolatedParameter

--- Source/InterpolatedParameter.hpp
+++ Source/InterpolatedParameter.hpp
@@ -9,6 +9,11 @@
 #define InterpolatedParameter_hpp
 
 
+/**
+ Represents a parameter that can gradually modulate between values.
+ Can set a current and target value, number of samples over which the modulation should occur,
+ as well as a point at which to start the modulation.
+ */
 class InterpolatedParameter
 {
 public:
@@ -19,19 +24,32 @@ public:
     /** Destructor. */
     ~InterpolatedParameter() {}
     
+    /** Updates the parameter value, based on the number of track samples processed.
+     
+     @param[in] currentSample Playhead position in track
+     @param[in] numSamples Number of audio samples processed since last update */
     void update(int currentSample, int numSamples);
     
     // Set startSample < 0 for instant change now
     // Set numSamples <= 0 for instant change at startSample
+    /** Specifies a new interpolation.
+     
+     @param[in] targetValue Parameter value to reach by end of modulation period
+     @param[in] startSample Position in track to start the modulation, in audio samples
+     @param[in] numSamples Length of modulation period, in audio samples*/
     void moveTo(double targetValue, int startSample = -1, int numSamples = -1);
     
+    /** Resets the current parameter value.
+     
+     @param[in] value New value for the parameter */
     void resetTo(double value);
     
-    double currentValue = 0.0;
-    double targetValue = 0.0;
-    double rateOfChange = 0.0;
-    int startSample = -1;
-    int samplesRemaining = -1;
+    double currentValue = 0.0; ///< Current value of the parameter
+    double targetValue = 0.0; ///< Target value that the parameter will reach after interpolation
+    double rateOfChange = 0.0; ///< Amount the value will change per audio sample
+    int startSample = -1; ///< Position in track to start the modulation, in audio samples
+    int samplesRemaining = -1; ///< Number of audio samples remaining before interpolation is complete
+    
 };
 
 #endif /* InterpolatedParameter_hpp */
--- Source/Track.hpp
+++ Source/Track.hpp
@@ -12,9 +12,12 @@
 #include "MixInfo.hpp"
 
 
-#define HIGH_PASS_MAX (150)
+#define HIGH_PASS_MAX (150) ///< Peak cut-off frequency of high-pass filtering during a crossfade
 
 
+/**
+ Holds data on the playback state of a track, as well as pointers to the general track information and its audio data.
+ */
 class Track
 {
 public:
@@ -25,35 +28,58 @@ public:
     /** Destructor. */
     ~Track() {}
     
-    // Returns true if current mix has finished
+    /** Updates the track state based on the number of audio samples that have been processed.
+     
+     @param[in] numSamples Number of samples processed since last update
+     
+     @return True if the current transition has finished */
     bool update(int numSamples);
     
-    // Returns false if there is no next track
+    /** Applies the provided mix information to the track state, readying it for the next transition.
+     
+     @param[in] mix Information on the next transition
+     
+     @return False if there is no track to transition to */
     bool applyNextMix(MixInfo* mix);
     
+    /** Fetches the current playhead position of the track.
+     
+     @return Current track position, in audio samples */
     int getPlayhead() { return playhead; }
     
+    /** Resets the track playhead to a given position.
+    
+    @param[in] sample Audio sample to set playhead to */
     void resetPlayhead(int sample) { playhead = sample; }
     
+    /** Resets the track state, with optional parameters to specify.
+     
+     @param[in] initBpm Initial tempo of track
+     @param[in] initGain Initial volume gain
+     @param[in] initHighPass Initial high-pass frequency */
     void reset(double initBpm = 0.0, double initGain = 0.0, double initHighPass = HIGH_PASS_MAX);
     
+    /** Fetches information on the current/next mix transition.
+     
+     @return Pointer to the current mix information */
     MixInfo* getCurrentMix() { return currentMix; }
     
-    TrackInfo* info = nullptr; ///< Pointer to information about this track
     
-    bool leader = false;
+    TrackInfo* info = nullptr; ///< Pointer to information about the currently loaded track
     
-    juce::AudioBuffer<float>* audio;
+    bool leader = false; ///< Indicates whether this track is currently leading the mix
     
-    InterpolatedParameter bpm;
-    InterpolatedParameter gain;
-    InterpolatedParameter highPassFreq;
+    juce::AudioBuffer<float>* audio; ///< Pointer to audio data for the loaded track
+    
+    InterpolatedParameter bpm; ///< Tempo parameter that can interpolate between its current and target values
+    InterpolatedParameter gain; ///< Gain parameter that can interpolate between its current and target values
+    InterpolatedParameter highPassFreq; ///< High-pass frequency parameter that can interpolate between its current and target values
     
 private:
     
-    MixInfo* currentMix;
+    MixInfo* currentMix; ///< Pointer to information on the current/next mix transition
     
-    int playhead = 0;
+    int playhead = 0; ///< Track playhead position, in audio samples
     
 };
 
